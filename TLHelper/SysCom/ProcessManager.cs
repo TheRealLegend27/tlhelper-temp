ee18f1a8a03bbd584a6b6eab45fddf51ee4a4f90
Ben Fischer <ben@fischer-enterprise.de>
2020-04-18 02:36:24 +0200

    Fix IsDiabloRunning to recognice the -launch version

--- TLHelper/SysCom/ProcessManager.cs
+++ TLHelper/SysCom/ProcessManager.cs
@@ -10,10 +10,11 @@ namespace TLHelper.SysCom
     public static class ProcessManager
     {
         private static readonly string DiabloProcessTitle = "Diablo III";
+        private static readonly string Diablo64ProcessTitle = "Diablo III64";
         private static readonly string TurboHUDProcessTitle = "TurboHUD";
 
         public static bool IsTurboHUDRunning => IsProcessRunning(TurboHUDProcessTitle);
-        public static bool IsDiabloRunning => IsProcessRunning(DiabloProcessTitle);
+        public static bool IsDiabloRunning => IsProcessRunning(DiabloProcessTitle) || IsProcessRunning(Diablo64ProcessTitle);
 
         public static bool IsProcessRunning(string procTitle)
         {
