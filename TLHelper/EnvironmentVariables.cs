9a3c8a9c3fe742cf086e723e60a06d351ec967bb
Ben Fischer <ben@fischer-enterprise.de>
2020-06-17 22:04:35 +0200

    Change version

--- TLHelper/EnvironmentVariables.cs
+++ TLHelper/EnvironmentVariables.cs
@@ -4,7 +4,7 @@ namespace TLHelper
 {
     public static class EnvironmentVariables
     {
-        public static readonly string CURRENT_VERSION = "a1.2.0";
+        public static readonly string CURRENT_VERSION = "a1.2.1";
         public static readonly string API_ROOT = "https://tlhelper.fischer-enterprise.de/api/";
         public static readonly string WEBSITE_LINK = "https://tlhelper.fischer-enterprise.de";
         public static readonly string CONFIG_DIR = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\TLHelper";
